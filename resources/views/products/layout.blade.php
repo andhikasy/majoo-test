132e237025d16661ea4091de2805e5d9ba0e37a0
Andhika Saifuddin Yahya <dhikadotcom@gmail.com>
2020-10-01 01:43:10 +0700

    Initial commit

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Majoo Tech</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body style="background-color: #edf7ef">
+  
+<div class="container">
+	<br/>
+	<br/>
+	<br/>
+    @yield('content')
+</div>
+   
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+</head>
+<body>
+
+  <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="/home">Majoo Teknologi Indonesia</a>
+  </nav>
+  <div class="container-fluid">
+    <br>
+    <h3>Product</h3>
+    <br>
+    <div class="card-deck">
+  <div class="card">
+    <img src="{{ asset('img/standard_repo.png') }}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Majoo Pro</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+    <a href="#" class="btn btn-primary">Beli Sekarang</a>
+    </div>
+  </div>
+  <div class="card">
+    <img src="{{ asset('img/standard_repo.png') }}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Majoo Advance</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+    <a href="#" class="btn btn-primary">Beli Sekarang</a>
+    </div>
+  </div>
+  <div class="card">
+    <img src="{{ asset('img/standard_repo.png') }}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Majoo Lifestyle</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+        <a href="#" class="btn btn-primary">Beli Sekarang</a>
+    </div>
+  </div>
+  <div class="card">
+    <img src="{{ asset('img/standard_repo.png') }}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Majoo Desktop</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+        <a href="#" class="btn btn-primary">Beli Sekarang</a>
+    </div>
+  </div>
+</div>
+</div>
+<footer class="footer mt-auto py-3">
+<div class="fixed-bottom">
+      <div class="container">
+        <span class="text-muted"><p align="center">2019 PT. Majoo Teknologi Indonesia</p></span>
+      </div>
+  </div>
+</footer>
+    
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
